ef2cf3893fdf6a71c9d10eaeb8525324488f76c9
Vanja Radovanović <elvanja@gmail.com>
2013-05-27 10:15:05 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ControllerCallbakStoreInSession::Application.config.secret_token = '7f96f8355a12b2d86f7848458fbee4501700163121c3073953b5fa3baef4db70c54aaf3a792ebefb5d5a846f3fb97c56f2335855fa3b893c2012c9bd1ab810f5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ControllerCallbakStoreInSession::Application.config.session_store :cookie_store, key: '_controller_callbak_store_in_session_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ControllerCallbakStoreInSession::Application.config.session_store :active_record_store
